adf6cf2fb6a27f8152c0d92751a38b58ee7fb6f4
Haletha <haletha@gmail.com>
2024-01-16 23:07:34 -0600

    Update files
    Updated media queries. Modified script and index based on testing.

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 document.onscroll = () => {
-    if(window.scrollY > 0) {
-        document.querySelector('.header').classList.add('active');
+    if (window.scrollY > 0) {
+      document.querySelector('.header').classList.add('active');
     } else
-        document.querySelector('.header').classList.remove('active');
-};
-
-document.onload = () => {
-    if(window.scrollY > 0) {
-        document.querySelector('.header').classList.add('active');
+      document.querySelector('.header').classList.remove('active');
+  };
+  
+  document.onload = () => {
+    if (window.scrollY > 0) {
+      document.querySelector('.header').classList.add('active');
     } else
-        document.querySelector('.header').classList.remove('active');
-};
-
+      document.querySelector('.header').classList.remove('active');
+  };
+  
